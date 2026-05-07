77bbde24fde47e4957c2ff317d8e583eb74826b1
Tharun-nagarjunapu <Tharun.nagarjunapu@koundinyasatech.com>
2026-05-07 11:05:18 +0530

    new files added1

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
  
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/wallet", require("./routes/walletRoutes"));
+app.use("/api/notifications",require("./controllers/notificationController"));
  
 app.get('/', (req, res) => {
   res.send('Server is running');
