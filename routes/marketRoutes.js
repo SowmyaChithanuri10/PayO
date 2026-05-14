c32a8ad758499676ed83bce2f673b5238a02a20e
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-14 15:56:40 +0530

    final working code4

--- routes/marketRoutes.js
+++ routes/marketRoutes.js
@@ -4,16 +4,6 @@ const marketController = require('../controllers/marketController');
 
 // Market overview routes
 router.get('/overview', marketController.getMarketOverview);
-router.get('/top', marketController.getTopCryptocurrencies);
-router.get('/global', marketController.getGlobalStats);
-router.get('/trending', marketController.getTrending);
-router.get('/movers', marketController.getTopMovers);
 
-// Individual coin routes
-router.get('/coin/:symbol', marketController.getCoinData);
-
-// WebSocket management
-router.post('/realtime/start', marketController.startRealtimeUpdates);
-router.post('/realtime/stop', marketController.stopRealtimeUpdates);
 
 module.exports = router;
