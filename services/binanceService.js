1a89fd2b05562c906c116c3f9b578a87ec8b153b
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-14 13:40:16 +0530

    market service file changed

--- services/binanceService.js
+++ services/binanceService.js
@@ -1,53 +1,57 @@
 const WebSocket = require("ws");
- 
+
 let latestPrices = {};
- 
+
 function connectBinance() {
-  const ws = new WebSocket("wss://stream.binance.com:9443/ws/!ticker@arr");
- 
+
+  console.log("connectBinance STARTED");
+
+  const ws = new WebSocket(
+    "wss://stream.binance.com:9443/ws/!ticker@arr"
+  );
+
   ws.on("open", () => {
-    console.log("Binance WebSocket Connected");
+    console.log(" Binance WebSocket Connected");
   });
- 
+
   ws.on("message", (data) => {
+
+    console.log(" Receiving Binance data");
+
     const parsed = JSON.parse(data);
- 
+
     parsed.forEach((coin) => {
+
       if (coin.s.endsWith("USDT")) {
+
         latestPrices[coin.s] = {
           symbol: coin.s,
           price: parseFloat(coin.c),
           changePercent: parseFloat(coin.P),
-          high: parseFloat(coin.h),
-          low: parseFloat(coin.l),
-          volume: parseFloat(coin.v),
-          time: Date.now()
         };
+
       }
+
     });
+
   });
- 
+
   ws.on("close", () => {
-    console.log("Binance WebSocket closed, reconnecting...");
-    setTimeout(connectBinance, 3000); // auto-reconnect
+    console.log(" WebSocket CLOSED");
   });
- 
+
   ws.on("error", (err) => {
-    console.log(" Binance error:", err.message);
+    console.log(" WebSocket ERROR");
+    console.log(err);
   });
+
 }
- 
-// getter (important)
+
 function getLatestPrices() {
   return Object.values(latestPrices);
 }
- 
-function getPrice(symbol) {
-  return latestPrices[symbol.toUpperCase()];
-}
- 
+
 module.exports = {
   connectBinance,
-  getLatestPrices,
-  getPrice
+  getLatestPrices
 };
