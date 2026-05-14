8b7e217a668dae532aaa15971ab6b4da108364f9
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-14 12:34:10 +0530

    websocket Added1

--- server.js
+++ server.js
@@ -2,53 +2,62 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const socketIo = require("socket.io");
-const {connectBinance,getLatestPrices} = require("./services/binanceService");
+
+const {
+  connectBinance,
+  getLatestPrices
+} = require("./services/binanceService");
+
 const connectDB = require("./config/db");
-require('./cron/walletCron');
-  
+
+require("./cron/walletCron");
+
 connectDB();
+
 const app = express();
 const server = http.createServer(app);
- 
+
 app.use(express.json());
 app.use(cors());
 
 const io = socketIo(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*"
+  }
 });
- 
-// start websocket
+
+// START BINANCE WEBSOCKET
 connectBinance();
 
 // SOCKET REALTIME PUSH
 io.on("connection", (socket) => {
   console.log("Client connected");
- 
+
   const interval = setInterval(() => {
     socket.emit("prices", getLatestPrices());
   }, 1000);
- 
+
   socket.on("disconnect", () => {
+    console.log("Client disconnected");
     clearInterval(interval);
   });
 });
- 
- 
+
+// ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/wallet", require("./routes/walletRoutes"));
-app.use("/api/notifications",require("./routes/notificationRoutes"));
-app.use("/api/marketinfo",require("./routes/marketRoutes"));
- 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
- 
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');  
-});
+app.use("/api/notifications", require("./routes/notificationRoutes"));
+app.use("/api/marketinfo", require("./routes/marketRoutes"));
 
 app.get("/", (req, res) => {
-  res.send(" Binance Live Server Running");
+  res.send("Binance Live Server Running");
 });
- 
-app.listen(3001, () => console.log("Server running on 3001"));
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// IMPORTANT
+server.listen(3001, () => {
+  console.log("Server running on 3001");
+});
