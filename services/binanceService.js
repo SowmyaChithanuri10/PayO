25af032689371cd6272ea077d125da2b8142f56f
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-14 13:46:42 +0530

    market service file changed1

--- services/binanceService.js
+++ services/binanceService.js
@@ -7,42 +7,39 @@ function connectBinance() {
   console.log("connectBinance STARTED");
 
   const ws = new WebSocket(
-    "wss://stream.binance.com:9443/ws/!ticker@arr"
+    "wss://stream.binance.com:9443/stream?streams=btcusdt@ticker/ethusdt@ticker/solusdt@ticker"
   );
 
   ws.on("open", () => {
-    console.log(" Binance WebSocket Connected");
+    console.log("Binance WebSocket Connected");
   });
 
   ws.on("message", (data) => {
 
-    console.log(" Receiving Binance data");
+    console.log("MESSAGE RECEIVED");
 
     const parsed = JSON.parse(data);
 
-    parsed.forEach((coin) => {
+    const coin = parsed.data;
 
-      if (coin.s.endsWith("USDT")) {
+    latestPrices[coin.s] = {
+      symbol: coin.s,
+      price: parseFloat(coin.c),
+      changePercent: parseFloat(coin.P),
+      high: parseFloat(coin.h),
+      low: parseFloat(coin.l),
+      volume: parseFloat(coin.v),
+      time: Date.now()
+    };
 
-        latestPrices[coin.s] = {
-          symbol: coin.s,
-          price: parseFloat(coin.c),
-          changePercent: parseFloat(coin.P),
-        };
-
-      }
-
-    });
-
-  });
-
-  ws.on("close", () => {
-    console.log(" WebSocket CLOSED");
   });
 
   ws.on("error", (err) => {
-    console.log(" WebSocket ERROR");
-    console.log(err);
+    console.log("WS ERROR", err.message);
+  });
+
+  ws.on("close", () => {
+    console.log("WS CLOSED");
   });
 
 }
