f8d02ce3b9e32d3f4e096e07d90133ec8e668eb0
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-21 14:25:18 +0530

    graphs api added4

--- routes/marketRoutes.js
+++ routes/marketRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const marketController = require('../controllers/marketController');
 
 // Main dashboard endpoint - returns ALL data shown in your images
-router.get('/dashboard/:symbol?', marketController.getDashboardData);
+router.get('/dashboard', controllerFunction);
+
+router.get('/dashboard/:symbol', controllerFunction);
 
 // Get all coins with descriptions (for market list)
 router.get('/all-coins', marketController.getAllCoinsWithDescriptions);
