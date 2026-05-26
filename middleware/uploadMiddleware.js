ee1332a84cf7a06219c451f4d3130e70be2ee0c8
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-05-26 15:33:14 +0530

    admin api added2

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,9 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadPath = "uploads/";
+
+// Create uploads folder if not exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
 
   filename: (req, file, cb) => {
