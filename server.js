c1ae22b653dd99dee1df7bcf3895280f8abde61d
chaluvadi-sreya <Chaluvadi.sreya@koundinyasatech.com>
2026-06-05 19:00:25 +0530

    final working code

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));      
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
