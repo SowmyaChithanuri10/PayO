33ec721b9606875a413f906a14fa507d68287a48
rajeshreddythuti <rajeshreddy.t@koundinyasatech.com>
2026-07-21 16:56:03 +0530

    add money

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1399,7 +1399,7 @@ import Header from '../components/header';
 export default function HomeScreen({ navigation }) {
   const [balanceVisible, setBalanceVisible] = useState(true);
   const [available, setAvailable] = useState('0');
-  const [avbRuppee, setAvbRuppee] = useState('0');
+  const [avbRuppee, setAvbRuppee] = useState('1000');
   const [expertCoins, setExpertCoins] = useState([]);
   const [marketNews, setMarketNews] = useState([]);
   const [newsCount, setNewsCount] = useState(3);
